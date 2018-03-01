4f794111a2385d917b652f04446291a0650b683e
Rohit Pagedar <rohit91us@gmail.com>
2018-02-28 22:17:50 -0500

    selection sort

--- sorting.c
+++ sorting.c
@@ -1,20 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #define ARR_LEN 12
 
-int main()
+int main(int argc, char* argv[])
 {
     int arr[ARR_LEN] = {7,28,2,17,19,21,15,31,22,4,14,25};
+    int choice = 0;
+    if (argc != 2) {
+        printf("\nEnter correct arguments\n"
+        "1. Bubble Sort\n2.Insertion Sort\n3.Selection Sort\n");
+        return 0;
+    }
+    choice = atoi(argv[1]);
+    if (choice < 1) {
+        return 0;
+    }
 
     printf("Original array :\n");
     print_array(arr, ARR_LEN);
-    printf("\n\n\n");
-    printf("************Bubble Sort*********************\n");
-    bubble_sort(arr, ARR_LEN);
-    printf("********************************************\n");
-    printf("\n\n\n");
-    printf("************Insertion Sort******************\n");
-    insertion_sort(arr, ARR_LEN);
-    printf("********************************************\n");
-    printf("\n\n\n");
+    printf("\n");
+    switch (choice) {
+        case 1 :
+        printf("************Bubble Sort*********************\n");
+        bubble_sort(arr, ARR_LEN);
+        printf("********************************************\n");
+        break;
+        case 2 :
+        printf("************Insertion Sort******************\n");
+        insertion_sort(arr, ARR_LEN);
+        printf("********************************************\n");
+        break;
+        case 3 :
+        printf("************Selection Sort******************\n");
+        selection_sort(arr, ARR_LEN);
+        printf("********************************************\n");
+        break;
+    }
+    return 0;
 }
--- utils.c
+++ utils.c
@@ -5,11 +5,17 @@
 void print_array(int a[], int len)
 {
     int i;
-    printf("Printing array:\n");
     for (i=0;i<len;i++) {
         printf("%d\t",a[i]);
     }
     printf("\n");
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
 }
 
 void bubble_sort(int a[], int len)
@@ -18,12 +24,13 @@ void bubble_sort(int a[], int len)
     for (i=0;i<len;i++) {
         for (j=i+1;j<len;j++) {
             if (a[i] > a[j]) {
-                int swap = a[i];
-                a[i] = a[j];
-                a[j] = swap;
+                swap(&a[i], &a[j]);
             }
         }
+        printf("\nStep %d:\n", i+1);
+        print_array(a, len);
     }
+    printf("\nFinal:\n");
     print_array(a,len);
     return;
 }
@@ -34,13 +41,30 @@ void insertion_sort(int a[], int len)
     for(i=1;i<len;i++) {
         j = i;
         while (j>0 && a[j-1] > a[j]) {
-            int temp = a[j-1];
-            a[j-1] = a[j];
-            a[j] = temp;
-            j--;
+            swap(&a[j], &a[j-1]);
         }
-        printf("\nStep %d:\n", i);
+        printf("Step %d:\n", i);
         print_array(a, len);
+    }
+    printf("\nFinal:\n");
+    print_array(a, len);
+}
+
+void selection_sort(int a[], int len) {
+    int i,j;
+    for(i=0;i<(len-1);i++) {
+        int min = i;
+        for(j=i+1;j<len;j++) {
+            if (a[j] < a[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            swap(&a[i], &a[min]);
+        }
+        printf("\nStep %d:\n", i+1);
+        print_array(a, len);
+        
     }
     printf("\nFinal:\n");
     print_array(a, len);
